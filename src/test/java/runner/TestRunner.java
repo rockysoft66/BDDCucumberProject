1eabe4e160c9cd66eceba42d364c65ff323e1d9e
DESKTOP-APCA3AA <rockysoft6@gmail.com>
2021-05-29 00:08:32 +0800

    New changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = {"D:\\TestProject\\New workplace\\Freecrm\\src\\test\\resources\\Features\\Log.feature"}, 
+@CucumberOptions( features = {"/Freecrm/src/test/resources/Features/Log.feature"}, 
 				glue = {"stepDefinitions","AppHooks"}, dryRun = false,monochrome = true,
 				plugin = { "pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"timeline:test-output-thread/"}
