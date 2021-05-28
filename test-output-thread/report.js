d596cfebad463ae49eb68d756988751b75dbe035
DESKTOP-APCA3AA <rockysoft6@gmail.com>
2021-05-29 00:08:32 +0800

    updating the changs

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "0fd69bde-27e0-45da-a84c-3db9e2a26f34",
-    "feature": "Login into my-store",
-    "scenario": "Login page Title",
-    "start": 1622216561057,
-    "group": 15,
-    "content": "",
-    "tags": "",
-    "end": 1622216573375,
-    "className": "passed"
-  },
-  {
-    "id": "0d003c6b-8d31-4177-8a4d-2f1c53d4d7df",
-    "feature": "Login into my-store",
-    "scenario": "Login page Log",
-    "start": 1622216573403,
-    "group": 15,
-    "content": "",
-    "tags": "",
-    "end": 1622216581034,
-    "className": "passed"
-  },
-  {
-    "id": "8841568b-4cfa-4e32-a9bf-793e6df6822e",
+    "id": "31e724db-5b2f-41ae-8505-303d02e44140",
     "feature": "Login into my-store",
     "scenario": "Login with validate creditials",
-    "start": 1622216581042,
-    "group": 15,
+    "start": 1622217283479,
+    "group": 13,
     "content": "",
     "tags": "",
-    "end": 1622216592638,
+    "end": 1622217294324,
+    "className": "passed"
+  },
+  {
+    "id": "75536e8e-1b7a-46fc-808c-5ffb97dbaae4",
+    "feature": "Login into my-store",
+    "scenario": "Login page Title",
+    "start": 1622217266719,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1622217276288,
+    "className": "passed"
+  },
+  {
+    "id": "a56c99e8-74da-4005-8365-42d3b5256c59",
+    "feature": "Login into my-store",
+    "scenario": "Login page Log",
+    "start": 1622217276312,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1622217283476,
     "className": "passed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 15,
+    "id": 13,
     "content": "Thread[pool-1-thread-1,5,main]"
   }
 ]);
